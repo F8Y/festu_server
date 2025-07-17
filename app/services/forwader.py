b46e1786803d82b521078e2f667ca66122a40d66
F8Y <batigoal277@gmail.com>
2025-07-17 21:37:32 +1000

    Кеш

--- app/services/forwader.py
+++ app/services/forwader.py
@@ -4,8 +4,17 @@ from app.dto.schedule_raw_dto import ScheduleRawHTMLDTO
 from app.services.html_parser import extract_week_schedule
 from app.services.week_parser import get_week_range
 from app.services.json_parser import parse_schedule_to_json
+from app.core.cache import schedule_cache as cache
+
+def make_cache_key(dto:ScheduleDTO) -> str:
+    return f"{dto.Time}_{dto.GroupID}"
 
 async def forward_schedule_to(dto: ScheduleDTO):
+    key = make_cache_key(dto)
+
+    if key in cache:
+        return cache[key]
+
     raw_dto = ScheduleRawHTMLDTO(Time=dto.Time, HTML=dto.HTML)
     filtered_html = extract_week_schedule(raw_dto.HTML, raw_dto.Time)
 
@@ -21,4 +30,6 @@ async def forward_schedule_to(dto: ScheduleDTO):
     monday, sunday = get_week_range(raw_dto.Time)
     structured_json = parse_schedule_to_json(filtered_html, monday, sunday)
 
+
+    cache[key] = structured_json
     return structured_json
