49485acdd2b555d3cff6600badc452e49f49ae51
F8Y <batigoal277@gmail.com>
2025-07-17 21:37:19 +1000

    Проверка наличия лимита на запросы у сервера

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 from app.core.factory import create_app
+from app.core.config import ENABLE_RATE_LIMIT
+from fastapi import Request
+from slowapi.middleware import SlowAPIMiddleware
+from app.core.limiter import limiter
 
 app = create_app()
+
+if ENABLE_RATE_LIMIT:
+    app.state.limiter = limiter
+    app.add_middleware(SlowAPIMiddleware)
