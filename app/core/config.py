254e8d6ce3c1050c8752d6b6a27e24aa3ec77eb7
F8Y <batigoal277@gmail.com>
2025-07-17 21:36:24 +1000

    + кеш, + лимиты запросов к серверу

--- app/core/config.py
+++ app/core/config.py
@@ -7,3 +7,9 @@ FESTU_ENDPOINT = os.getenv("FESTU_ENDPOINT")
 
 if not FESTU_ENDPOINT:
     raise RuntimeError('FESTU_ENDPOINT not set in .env')
+
+CACHE_TTL = int(os.getenv("CACHE_TTL", 600))
+CACHE_MAXSIZE = int(os.getenv("CACHE_MAXSIZE", 100))
+
+RATE_LIMIT = os.getenv("RATE_LIMIT", "60/hour")
+ENABLE_RATE_LIMIT = os.getenv("ENABLE_RATE_LIMIT", "true").lower() == "true"
