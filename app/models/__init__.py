1d59395ba2e69446f627f80407d2b4f94897496c
F8Y <batigoal277@gmail.com>
2025-10-20 21:51:53 +1000

    add: initialization as packages

---
+++ app/models/__init__.py
@@ -0,0 +1,14 @@
+from .institute import InstituteResponse, InstituteListResponse, GroupResponse, GroupListResponse
+from .schedule import ScheduleResponse, DaySchedule, PairInfo, TeacherInfo, ScheduleRequest
+
+__all__ = [
+    "InstituteResponse",
+    "InstituteListResponse",
+    "GroupResponse",
+    "GroupListResponse",
+    "ScheduleResponse",
+    "DaySchedule",
+    "PairInfo",
+    "TeacherInfo",
+    "ScheduleRequest"
+]
