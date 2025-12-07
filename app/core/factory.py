e8e6b5ab2f134e2075f08c8f49a7aedcdeb12a96
F8Y <batigoal277@gmail.com>
2025-12-07 21:03:43 +1000

    add CORS parameters

--- app/core/factory.py
+++ app/core/factory.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from app.api import institutes, schedule
 from datetime import datetime
+from fastapi.middleware.cors import CORSMiddleware
 
 
 def create_app() -> FastAPI:
@@ -11,6 +12,19 @@ def create_app() -> FastAPI:
         version="1.0.0",
         docs_url="/docs",
         redoc_url="/redoc"
+    )
+
+    origins = [
+        'http://localhost:3000',
+        'http://127.0.0.1:3000'
+    ]
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=['*'],
+        allow_headers=['*'],
     )
 
     app.include_router(
